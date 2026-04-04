fbc3b177b64a3992be1393c29c13fcd73aaa8ff3
Christophe <delclos.c13@gmail.com>
2026-04-04 14:34:51 +0200

    modifications icones

--- src/data/techIcons.js
+++ src/data/techIcons.js
@@ -1,9 +1,9 @@
 export const techIcons = {
-  "React": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-  "Node.js": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-  "MongoDB": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
-  "Sass": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
-  "Vite": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vitejs/vitejs-original.svg",
-  "HTML": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-  "CSS": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+  "React": "/assets/tech/react.svg",
+  "Node.js": "/assets/tech/node.svg",
+  "MongoDB": "/assets/tech/mongodb.svg",
+  "Sass": "/assets/tech/sass.svg",
+  "Vite": "/assets/tech/vite.svg",
+  "HTML": "/assets/tech/html5.svg",
+  "CSS": "/assets/tech/css.svg",
 };
